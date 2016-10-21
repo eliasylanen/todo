09d252358ceb88d3d413c8c07dddd9188249abef
Elias Ylänen <elias.ylanen@gmail.com>
2016-10-21 16:01:53 +0300

    Release 1.0.0

--- app/client/src/appFactory.js
+++ app/client/src/appFactory.js
@@ -15,6 +15,8 @@ function appFactory($http) {
     },
     loginOrRegister,
     getList,
+    addItem,
+    deleteItem,
   };
 
   function loginOrRegister(url, username, password) {
@@ -31,6 +33,31 @@ function appFactory($http) {
     return $http({
       method: 'GET',
       url: '/api/lists',
+      headers: { 'x-access-token': token },
+    });
+  }
+
+  function addItem(data) {
+    if (data._id) {
+      return $http({
+        method: 'PUT',
+        url: `/api/lists/${data._id}`,
+        headers: { 'x-access-token': token },
+        data,
+      });
+    }
+    return $http({
+      method: 'POST',
+      url: '/api/lists',
+      headers: { 'x-access-token': token },
+      data,
+    });
+  }
+
+  function deleteItem(id) {
+    return $http({
+      method: 'DELETE',
+      url: `/api/lists/${id}`,
       headers: { 'x-access-token': token },
     });
   }
--- app/client/src/listCtrl.js
+++ app/client/src/listCtrl.js
@@ -8,9 +8,18 @@ function listCtrl(appFactory) {
 
   vm.token = appFactory.token;
   vm.listItems = null;
+  vm.itemToEdit = null;
+  vm.sortByThis = 'item.done';
+  vm.reverse = false;
 
   vm.init = init;
+  vm.addItem = addItem;
+  vm.editItem = editItem;
+  vm.deleteItem = deleteItem;
+  vm.save = save;
+  vm.cancel = cancel;
   vm.logout = logout;
+  vm.sort = sort;
 
   function init() {
     if (!vm.token) window.location.replace('/');
@@ -22,9 +31,56 @@ function listCtrl(appFactory) {
     $('.button-collapse').sideNav();
   }
 
+  function addItem() {
+    $('#addEdit').slideToggle('slow');
+    vm.itemToEdit = {
+      done: false,
+      content: null,
+      priority: null,
+    };
+  }
+
+  function editItem(id) {
+    vm.itemToEdit = vm.listItems.find((value) => value._id === id);
+    $('#addEdit').slideToggle('slow');
+  }
+
+  function deleteItem(id) {
+    appFactory.deleteItem(id)
+    .then(() => appFactory.getList())
+    .then((data) => {
+      vm.listItems = data.data.msg[0].item;
+    })
+    .catch(err => { alert(err); });
+  }
+
+  function save() {
+    if (vm.itemToEdit.content && vm.itemToEdit.priority
+        && (vm.itemToEdit.priority <= 3 && vm.itemToEdit.priority >= 1)) {
+      appFactory.addItem(vm.itemToEdit)
+      .then(() => appFactory.getList())
+      .then((data) => {
+        vm.listItems = data.data.msg[0].item;
+      })
+      .catch(err => { alert(err); });
+      $('#addEdit').slideToggle('slow');
+    } else {
+      alert('Please input correct values to both content and priority');
+    }
+  }
+
+  function cancel() {
+    $('#addEdit').slideToggle('slow');
+  }
+
   function logout() {
     localStorage.removeItem('todoToken');
     sessionStorage.removeItem('todoToken');
     window.location.replace('/');
+  }
+
+  function sort(sortByThis) {
+    vm.reverse = (vm.sortByThis === sortByThis) ? !vm.reverse : false;
+    vm.sortByThis = sortByThis;
   }
 }
